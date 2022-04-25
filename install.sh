630adfb631043a0c1ec591113c7bea6caad5cf2f
m0nk3h <butmonkeh@gmail.com>
2022-04-25 20:30:57 +0100

    add asusgetmode

--- install.sh
+++ install.sh
@@ -5,6 +5,8 @@ if (( EUID != 0 )); then
 fi
 
 cp asusctltray /usr/local/bin
+cp asusgetmode /usr/local/bin
 chmod a+x /usr/local/bin/asusctltray
+chmod a+x /usr/local/bin/asusgetmode
 cp asusctltray.svg /usr/share/pixmaps
 cp asusctltray.desktop /usr/share/applications
